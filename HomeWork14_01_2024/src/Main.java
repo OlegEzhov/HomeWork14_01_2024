1b9cf7d479277639f646d6e7a304b9077fc64b61
OlegEzhov <127249557+OlegEzhov@users.noreply.github.com>
2024-01-14 14:44:36 +0100

    Add files via upload

---
+++ HomeWork14_01_2024/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int number1 = 126;
+        int number2 = 8;
+
+        int result = number1 + number2;
+        int result2 = number1 - number2;
+        int result3 = number1 / number2;
+        int result4 = number1 * number2;
+
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+
+    }
+}
---
+++ HomeWork_14_01_2024/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int q = 55978;
+        byte w = 125;
+        short e = 24532;
+        char r = '@';
+        long t = 214125415;
+        String y = "Hello";
+
+        System.out.println(q);
+        System.out.println(w);
+        System.out.println(e);
+        System.out.println(r);
+        System.out.println(t);
+        System.out.println(y);
+
+
+    }
+}
